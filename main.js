86cd1da3657098fc7a24fe7b030f631373bed983
nparsley <nickparsley5@gmail.com>
2020-12-22 19:03:39 -0700

    city,weather api fetch

---
+++ main.js
@@ -0,0 +1,29 @@
+const api = {
+    key: "79d6c4d42f9450966b7c67a449ed5e44",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+}
+
+
+
+
